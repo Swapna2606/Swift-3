3479eb9f33dac0db295956ce6fd6d2b392ef950e
raunek kantharia <raunekk@rauneks-iMac.local>
2017-04-21 17:09:50 +0530

    Initial Commit

---
+++ Marine Insight iOSTests/Marine_Insight_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Marine_Insight_iOSTests.swift
+//  Marine Insight iOSTests
+//
+//  Created by raunek kantharia on 21/04/17.
+//  Copyright © 2017 marineinsight. All rights reserved.
+//
+
+import XCTest
+@testable import Marine_Insight_iOS
+
+class Marine_Insight_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
