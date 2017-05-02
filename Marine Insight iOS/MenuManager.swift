7ff37c4de4a62763ca2a1bc974d17d1199058b2e
raunek kantharia <raunekk@rauneks-iMac.local>
2017-05-02 16:02:15 +0530

    Adding Menu Feature

---
+++ Marine Insight iOS/MenuManager.swift
@@ -0,0 +1,13 @@
+//
+//  MenuManager.swift
+//  Marine Insight iOS
+//
+//  Created by raunek kantharia on 02/05/17.
+//  Copyright © 2017 marineinsight. All rights reserved.
+//
+
+import UIKit
+
+class MenuManager: NSObject {
+
+}
