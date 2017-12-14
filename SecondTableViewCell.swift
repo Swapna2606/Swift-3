c4d381fe02d9d9861a6fe8f56eea0f12b30f4b0f
Swapna Kheni <swapnalekshmanan@gmail.com>
2017-12-14 17:28:23 -0500

    master push

---
+++ SecondTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SecondTableViewCell.swift
+//  Marine Insight iOS
+//
+//  Created by raunek kantharia on 29/04/17.
+//  Copyright © 2017 marineinsight. All rights reserved.
+//
+
+import UIKit
+
+class SecondTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
